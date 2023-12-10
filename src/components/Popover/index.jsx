baf027ab877fd56961861d307a349c2f5f9240b2
Tal Kohavy <talkohavy@gmail.com>
2023-12-11 00:53:08 +0200

    added a Popover component

---
+++ src/components/Popover/index.jsx
@@ -0,0 +1,18 @@
+import * as PopoverOriginal from '@radix-ui/react-popover';
+
+export default function Popover() {
+  return (
+    <PopoverOriginal.Root>
+      <PopoverOriginal.Trigger className='inline-flex h-9 items-center justify-center rounded border border-transparent bg-white px-4 font-medium shadow-xs hover:bg-neutral-100 focus:border-black dark:bg-slate-800 dark:shadow-dark-xs dark:hover:bg-neutral-800'>
+        More info
+      </PopoverOriginal.Trigger>
+
+      <PopoverOriginal.Portal>
+        <PopoverOriginal.Content className='w-64 rounded bg-white p-5 shadow-xs dark:bg-slate-800 dark:text-white dark:shadow-dark-xs'>
+          Some more info…
+          <PopoverOriginal.Arrow className='fill-white dark:fill-slate-800' />
+        </PopoverOriginal.Content>
+      </PopoverOriginal.Portal>
+    </PopoverOriginal.Root>
+  );
+}
