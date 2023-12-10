977f388c951a7bda6e49652072e5430926ae4502
Tal Kohavy <talkohavy@gmail.com>
2023-12-10 21:24:35 +0200

    added tailwind into the mix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: `${process.cwd()}/src`, // <--- where the index.html is located. defaults to `process.cwd()`.
   plugins: [react()],
+  publicDir: './public', // default is "public". The location of the public dir relative to the index.html file.
   server: {
     open: true,
     port: 3000,
