0ac3a20d7d1dcc6bff759f288b08d78e6aa98dc3
Tal Kohavy <talkohavy@gmail.com>
2023-12-11 01:27:07 +0200

    added a Tooltip component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as TooltipOriginal from '@radix-ui/react-tooltip';
 import App from './App';
 import DarkThemeProvider from './providers/DarkThemeProvider';
 import './index.css';
@@ -7,7 +8,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DarkThemeProvider>
-      <App />
+      <TooltipOriginal.Provider delayDuration={800} skipDelayDuration={500}>
+        <App />
+      </TooltipOriginal.Provider>
     </DarkThemeProvider>
   </React.StrictMode>,
 );
