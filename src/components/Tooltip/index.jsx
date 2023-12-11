516ea573d5b9f3d5581a80be7f6ecfbe83fcebe5
Tal Kohavy <talkohavy@gmail.com>
2023-12-11 10:33:35 +0200

    organized components a little better

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -2,14 +2,17 @@ import * as TooltipOriginal from '@radix-ui/react-tooltip';
 
 export default function Tooltip({ content, children }) {
   return (
-    <TooltipOriginal.Provider delayDuration={800} skipDelayDuration={500}>
+    <TooltipOriginal.Provider delayDuration={600} skipDelayDuration={2000}>
       <TooltipOriginal.Root>
         <TooltipOriginal.Trigger asChild>{children}</TooltipOriginal.Trigger>
 
         <TooltipOriginal.Portal>
-          <TooltipOriginal.Content className='tooltip-content max-w-lg' sideOffset={5}>
+          <TooltipOriginal.Content
+            className='tooltip-content z-10 max-w-lg rounded border border-black bg-white p-2'
+            sideOffset={5}
+          >
             {content}
-            <TooltipOriginal.Arrow />
+            <TooltipOriginal.Arrow className='fill-gray-600' />
           </TooltipOriginal.Content>
         </TooltipOriginal.Portal>
       </TooltipOriginal.Root>
