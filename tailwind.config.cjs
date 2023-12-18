d1b523fe03e71252fd317948ecb719d933484bc8
Tal Kohavy <talkohavy@gmail.com>
2023-12-19 01:04:44 +0200

    added a 2xs shadow box to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -213,6 +213,7 @@ module.exports = {
         '4xl': '2rem', // use like so: rounded-4xl
       },
       boxShadow: {
+        '2xs': '0 0 1px 1px rgba(0, 0, 0, 0.1)',
         xs: '0 0 2px 2px rgba(0, 0, 0, 0.1)',
         sm: '0 0 4px 4px rgba(0, 0, 0, 0.1)',
         md: '0 0 6px 6px rgba(0, 0, 0, 0.1)',
