3ec9a9a0b3b7b49088a8a6e913dec117138eb63f
Tal Kohavy <talkohavy@gmail.com>
2024-02-03 13:10:53 +0200

    small

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: `${process.cwd()}/src`, // <--- where the index.html is located. defaults to `process.cwd()`.
+  root: `${process.cwd()}/src`, // <--- defaults to process.cwd(). where the index.html is located.
   plugins: [react()],
-  publicDir: './public', // default is "public". The location of the public dir relative to the index.html file.
+  publicDir: './public', // defaults to "public". The location of the public dir relative to the index.html file.
   server: {
     open: true,
     port: 3000,
