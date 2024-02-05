ee30ebae0a306c2518eedeb1e5322a2b27db392d
Tal Kohavy <talkohavy@gmail.com>
2024-02-05 23:23:04 +0200

    changed header from dov to header

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -2,8 +2,8 @@ import DarkModeToggle from '../../DarkModeToggle';
 
 export default function Header() {
   return (
-    <div className='flex h-15 items-center justify-start bg-slate-400 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
+    <header className='flex h-15 items-center justify-start bg-slate-400 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
       <DarkModeToggle size={20} />
-    </div>
+    </header>
   );
 }
