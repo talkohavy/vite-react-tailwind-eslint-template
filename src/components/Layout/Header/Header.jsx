b23ffbec0d56331ba27eb54452aed2c765282ae0
Tal Kohavy <talkohavy@gmail.com>
2024-04-24 00:11:51 +0300

    Header is now 1 z-index above Sidebar

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,7 +2,8 @@ import DarkModeToggle from '../../DarkModeToggle';
 
 export default function Header() {
   return (
-    <header className='relative flex h-15 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
+    // z-index of Header needs to be 1 more than that of SideBar, in order for Header to cast shadow on it.
+    <header className='relative z-10 flex h-15 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
       <DarkModeToggle size={20} />
     </header>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div>
+    <div className='size-full p-6'>
       <div>Main</div>
       <div>window</div>
     </div>
