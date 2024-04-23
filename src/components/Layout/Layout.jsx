dc26732c2de1579aa8c0d0cf5608a6a67c24f7d5
Tal Kohavy <talkohavy@gmail.com>
2024-04-24 00:35:49 +0300

    replaced z-index with overflow-auto on header to make shadow disappear

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <div className='flex h-full flex-col items-start justify-start'>
       <Header />
 
-      <div className='flex w-full flex-grow items-center justify-center'>
+      <div className='flex size-full items-center justify-center overflow-auto'>
         <Sidebar />
 
         <Main>{children}</Main>
