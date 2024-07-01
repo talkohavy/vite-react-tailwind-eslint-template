93baea625437c0077dab4b2294a1a0373f7bf580
Tal Kohavy <talkohavy@gmail.com>
2024-07-01 09:48:58 +0300

    tsconfig includes updated

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,13 @@
+import type { PropsWithChildren } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Sidebar from './Sidebar';
 
-export default function Layout({ children }) {
+type LayoutProps = PropsWithChildren;
+
+export default function Layout(props: LayoutProps) {
+  const { children } = props;
+
   return (
     <div className='flex h-full flex-col items-start justify-start'>
       <Header />
