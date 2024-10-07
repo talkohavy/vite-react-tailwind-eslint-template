7fa942aa8c2b35448a994c6c6f70425bd32b9252
Tal Kohavy <talkohavy@gmail.com>
2024-10-07 19:27:22 +0300

    SuspenseUntilReady supports both a function as well as a component

--- src/components/SuspenseUntilReady/SuspenseUntilReady.tsx
+++ src/components/SuspenseUntilReady/SuspenseUntilReady.tsx
@@ -1,11 +1,12 @@
-import { type PropsWithChildren, useEffect, useState } from 'react';
+import { type PropsWithChildren, type ReactNode, useEffect, useState } from 'react';
 
 type SuspenseUntilReadyProps = PropsWithChildren<{
   asyncFn: () => Promise<void>;
+  Loader?: ReactNode | (() => ReactNode);
 }>;
 
 export default function SuspenseUntilReady(props: SuspenseUntilReadyProps) {
-  const { children, asyncFn } = props;
+  const { children, asyncFn, Loader } = props;
 
   const [isReady, setIsReady] = useState<boolean>();
 
@@ -13,7 +14,15 @@ export default function SuspenseUntilReady(props: SuspenseUntilReadyProps) {
     asyncFn().then(() => setIsReady(true));
   }, [asyncFn]);
 
-  if (!isReady) return null;
+  if (!isReady) {
+    if (Loader) {
+      if (typeof Loader === 'function') return <Loader />;
+
+      return Loader;
+    }
+
+    return null;
+  }
 
   return children;
 }
