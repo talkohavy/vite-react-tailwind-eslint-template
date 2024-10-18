72e157609fd61f8c0094c1cb6f6fe2f4dfdafa0c
Tal Kohavy <talkohavy@gmail.com>
2024-10-18 22:57:04 +0300

    added the useEventListener hook

---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from 'react';
+
+type UseEventListenerProps = {
+  eventType: string;
+  fnToRun: (props?: any) => void;
+  dependencies?: Array<any>;
+  element?: any;
+  /**
+   * @default true
+   */
+  shouldExecute?: boolean;
+};
+
+export default function useEventListener(props: UseEventListenerProps) {
+  const { eventType, fnToRun, dependencies = [], element = window, shouldExecute = true } = props;
+
+  const callbackRef = useRef(fnToRun);
+
+  useEffect(() => {
+    callbackRef.current = fnToRun;
+  }, [fnToRun]);
+
+  useEffect(() => {
+    if (!shouldExecute) return;
+
+    element.addEventListener(eventType, callbackRef.current);
+
+    callbackRef.current();
+
+    return () => element.removeEventListener(eventType, callbackRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...dependencies, shouldExecute]);
+}
