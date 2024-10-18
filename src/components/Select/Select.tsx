f17e3fff8686f8abba602ac0c0a7cba0758d7377
Tal Kohavy <talkohavy@gmail.com>
2024-10-18 22:56:17 +0300

    Select - better types

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,12 +1,10 @@
-import type { ReactNode } from 'react';
 import clsx from 'clsx';
-
-type Option = { value: string | number; label: ReactNode };
+import { SelectOption } from './types';
 
 type SelectProps = {
-  selectedOption: Option;
-  setOption: any;
-  options: Array<Option>;
+  selectedOption: SelectOption;
+  setOption: (value: SelectOption) => void;
+  options: Array<SelectOption>;
   className?: string;
 };
 
@@ -19,10 +17,13 @@ export default function Select(props: SelectProps) {
       onChange={(e) => {
         const selectedValue = e.target.value;
         const selectedOption = options.find((option) => option.value.toString() === selectedValue.toString());
+
+        if (!selectedOption) throw new Error("could not find `selectedValue` inside of Select's options array");
+
         setOption(selectedOption);
       }}
       className={clsx(
-        'h-8 cursor-pointer rounded-md border p-1 hover:border-blue-400 focus:border-blue-600',
+        'h-8 cursor-pointer rounded-md border p-1 hover:border-blue-400 focus:border-blue-600 dark:bg-slate-900',
         className,
       )}
     >
---
+++ src/components/Select/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+
+export type SelectOption = {
+  value: string | number;
+  label: ReactNode;
+};
