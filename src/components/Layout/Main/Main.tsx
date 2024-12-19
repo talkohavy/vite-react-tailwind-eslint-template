170c2571cec72c77c9ebf991473dccd2291ad4d0
Tal Kohavy <talkohavy@gmail.com>
2024-12-19 09:16:47 +0200

    text color defaults to white in dark-mode

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -3,5 +3,9 @@ import type { PropsWithChildren } from 'react';
 export default function Main(props: PropsWithChildren) {
   const { children } = props;
 
-  return <main className='flex size-full items-center justify-between bg-white dark:bg-[#383838]'>{children}</main>;
+  return (
+    <main className='flex size-full items-center justify-between bg-white dark:bg-[#383838] dark:text-white'>
+      {children}
+    </main>
+  );
 }
