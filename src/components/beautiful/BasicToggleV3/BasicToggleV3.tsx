68927d5ab68ba707ac98923b347a8e01b812353f
Tal Kohavy <talkohavy@gmail.com>
2024-12-21 17:20:02 +0200

    added to beautiful: BasicToggleV3

---
+++ src/components/beautiful/BasicToggleV3/BasicToggleV3.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import styles from './BasicToggleV3.module.css';
+
+type BasicToggleV3Props = {
+  isChecked: boolean;
+  setIsChecked: (value: any) => void;
+  disabled?: boolean;
+  className?: string;
+};
+
+export default function BasicToggleV3(props: BasicToggleV3Props) {
+  const { isChecked, setIsChecked, disabled, className } = props;
+
+  return (
+    <label className={clsx(styles.switch, className)}>
+      <input type='checkbox' checked={isChecked} onChange={setIsChecked} disabled={disabled} />
+      <span className={styles.slider}>
+        <div className={styles.roundBall} />
+      </span>
+    </label>
+  );
+}
