ef5fda750362a51fe4370bbe0b379ea9f0e51fb7
Tal Kohavy <talkohavy@gmail.com>
2024-12-24 01:52:06 +0200

    added more themes to highlight

--- src/lib/syntaxHighlighter/index.ts
+++ src/lib/syntaxHighlighter/index.ts
@@ -2,6 +2,7 @@ import hljs from 'highlight.js';
 import { typescriptLanguageToRegister } from './languages/typescript';
 import { javascriptLanguageToRegister } from './languages/javascript';
 import './styles/vscode-dark.css';
+import './styles/1c-light.css';
 
 hljs.registerLanguage('javascript', javascriptLanguageToRegister);
 hljs.registerLanguage('typescript', typescriptLanguageToRegister);
