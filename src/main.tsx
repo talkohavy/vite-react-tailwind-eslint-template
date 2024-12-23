4d089ed668f854f4ea9a6c02fd53777c29b8dbd8
Tal Kohavy <talkohavy@gmail.com>
2024-12-24 00:55:39 +0200

    main.tsx is now using bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,15 @@
 import { StrictMode } from 'react';
-import axios from 'axios';
 import { createRoot } from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router';
 import App from '@src/App';
 import SuspenseUntilReady from './components/SuspenseUntilReady';
-import { initDAL } from './DAL';
 import DarkThemeProvider from './providers/DarkThemeProvider';
 import { createStore } from './store';
+import './common/bootstrap';
 import './index.css';
 
-const httpClientAxios = axios.create({ baseURL: 'http://localhost:8000', withCredentials: true });
-
 const store = createStore({} as any);
-
-initDAL({ httpClient: httpClientAxios });
 
 function Client() {
   return (
