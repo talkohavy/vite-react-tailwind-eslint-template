abf7715dd924b1f0ef445fbb7bef85c0f9462d16
Tal Kohavy <talkohavy@gmail.com>
2024-12-24 00:33:27 +0200

    added pt-1 to Main component so that on scroll, the Header's shadow won't get covered

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -4,7 +4,7 @@ export default function Main(props: PropsWithChildren) {
   const { children } = props;
 
   return (
-    <main className='flex size-full items-center justify-between bg-white dark:bg-[#212121] dark:text-white'>
+    <main className='flex size-full items-center justify-between bg-white pt-1 dark:bg-[#212121] dark:text-white'>
       {children}
     </main>
   );
