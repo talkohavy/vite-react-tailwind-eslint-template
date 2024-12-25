cc55af4c6d73567d15ae7b4a318cafdbd8acc501
Tal Kohavy <talkohavy@gmail.com>
2024-12-25 09:35:52 +0200

    FancyRadio refactored

--- src/components/beautiful/FancyRadio/FancyRadio.tsx
+++ src/components/beautiful/FancyRadio/FancyRadio.tsx
@@ -5,17 +5,18 @@ type FancyRadioProps = {
   isChecked: boolean;
   setIsChecked: (value: any) => void;
   disabled?: boolean;
+  className?: string;
 };
 
 export default function FancyRadio(props: FancyRadioProps) {
-  const { isChecked, setIsChecked, disabled } = props;
+  const { isChecked, setIsChecked, disabled, className } = props;
 
   return (
-    <label className={styles.fancyRadio}>
+    <label className={clsx(styles.fancyRadio, className)}>
       <div className={styles.fancyRadioInnerStyle}>
         <input type='checkbox' checked={!!isChecked} onChange={setIsChecked} disabled={disabled} />
 
-        <div className={styles.blackRoundToggle} />
+        <div className={styles.slider} />
 
         <div className={clsx(styles.statusIndicatorStyle, styles.statusOn)} />
         <div className={clsx(styles.statusIndicatorStyle, styles.statusOff)} />
