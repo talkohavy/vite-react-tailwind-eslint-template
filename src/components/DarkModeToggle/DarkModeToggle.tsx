0abd2a8e7dcc5245037dc9a461a0ed03c7fcebd4
Tal Kohavy <talkohavy@gmail.com>
2024-12-25 08:40:04 +0200

    header is now using a simple radio button from controls

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,15 +1,14 @@
 import { useDarkTheme } from '../../providers/DarkThemeProvider/DarkThemeContext';
-import ToggleButton from '../ToggleButton';
+import Toggle from '../controls/Toggle';
 
 type DarkModeToggleProps = {
-  size?: number;
   className?: string;
 };
 
 export default function DarkModeToggle(props: DarkModeToggleProps) {
-  const { size, className } = props;
+  const { className } = props;
 
   const { isDarkMode, toggleDarkMode } = useDarkTheme();
 
-  return <ToggleButton value={isDarkMode} setValue={toggleDarkMode} size={size} className={className} />;
+  return <Toggle isChecked={isDarkMode} setIsChecked={toggleDarkMode} className={className} />;
 }
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,8 +2,8 @@ import DarkModeToggle from '../../DarkModeToggle';
 
 export default function Header() {
   return (
-    <header className='relative flex h-15 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
-      <DarkModeToggle size={20} />
+    <header className='relative flex h-15 px-3 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
+      <DarkModeToggle />
     </header>
   );
 }
