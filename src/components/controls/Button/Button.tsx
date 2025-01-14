653bdd430387d139fe7ad21d369c7f37b6933b68
Tal Kohavy <talkohavy@gmail.com>
2025-01-14 23:48:47 +0200

    Button element can have rest

--- src/components/controls/Button/Button.tsx
+++ src/components/controls/Button/Button.tsx
@@ -1,19 +1,24 @@
-import { PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type ButtonProps = PropsWithChildren<{
   onClick: () => void;
   className?: string;
 }>;
+// & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: ButtonProps) {
-  const { children, onClick, className } = props;
+  const { children, onClick, className, ...rest } = props;
 
   return (
     <button
       type='button'
       onClick={onClick}
-      className={twMerge('rounded-md hover:rounded-lg bg-blue-500 hover:bg-blue-600 active:bg-blue-500 p-3', className)}
+      className={twMerge(
+        'rounded-md hover:rounded-lg bg-blue-500 hover:bg-blue-600 active:bg-blue-500 focus:rounded-2xl p-3',
+        className,
+      )}
+      {...rest}
     >
       {children}
     </button>
