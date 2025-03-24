5f4ab6d737fea8675399bf9c2d02c6607e9a7a66
Tal Kohavy <talkohavy@gmail.com>
2025-03-24 11:46:51 +0200

    removed icon warning

--- src/components/svgs/CalendarIcon/CalendarIcon.tsx
+++ src/components/svgs/CalendarIcon/CalendarIcon.tsx
@@ -10,9 +10,9 @@ export default function CalendarIcon(props: CalendarIconProps) {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       xmlns='http://www.w3.org/2000/svg'
       className={className}
     >
