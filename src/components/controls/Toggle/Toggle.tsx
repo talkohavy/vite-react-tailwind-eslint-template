f84fdc8318f1e837b0af8ed5c5b647fc74779fb8
Tal Kohavy <talkohavy@gmail.com>
2025-03-31 23:05:42 +0300

    toggle now has supports a label

--- src/components/controls/Toggle/Toggle.tsx
+++ src/components/controls/Toggle/Toggle.tsx
@@ -5,27 +5,33 @@ type ToggleProps = {
   isChecked: boolean;
   setIsChecked: (value: any) => void;
   disabled?: boolean;
+  label?: string;
   className?: string;
 };
 
 export default function Toggle(props: ToggleProps) {
-  const { isChecked, setIsChecked, disabled, className } = props;
+  const { isChecked, setIsChecked, disabled, label, className } = props;
 
   return (
     <button type='button' role='switch' aria-checked={isChecked ? 'true' : 'false'}>
-      <label className={clsx(styles.switch, className)}>
-        <input
-          type='checkbox'
-          checked={isChecked}
-          onChange={setIsChecked}
-          disabled={disabled}
-          aria-hidden='true'
-          tabIndex={-1}
-        />
+      <label className={clsx(styles.label, className)}>
+        <div className={styles.checkboxInputAndFakeCheckMark}>
+          <input
+            type='checkbox'
+            checked={isChecked}
+            onChange={setIsChecked}
+            disabled={disabled}
+            aria-hidden='true'
+            tabIndex={-1}
+            className={styles.input}
+          />
 
-        <span className={styles.radioButtonBg}>
-          <div className={styles.slider} />
-        </span>
+          <span className={styles.fakeToggleHolder}>
+            <div className={styles.toggleCheckMark} />
+          </span>
+        </div>
+
+        {label && <div>{label}</div>}
       </label>
     </button>
   );
