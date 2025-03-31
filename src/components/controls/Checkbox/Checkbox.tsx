6341ef744e816d12c983eb279ebcef0fa5e43c39
Tal Kohavy <talkohavy@gmail.com>
2025-03-31 09:05:02 +0300

    Checkbox was separated to CheckboxLogic and style

--- src/components/controls/Checkbox/Checkbox.tsx
+++ src/components/controls/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
-import clsx from 'clsx';
 import CheckIcon from '../../svgs/CheckIcon';
+import styles from './Checkbox.module.scss';
+import CheckboxLogic from './CheckboxLogic';
 
 export type CheckboxProps = {
   isChecked: boolean;
@@ -11,36 +12,11 @@ export type CheckboxProps = {
 };
 
 export default function Checkbox(props: CheckboxProps) {
-  const { isChecked, setIsChecked, disabled, label, className, testId = '' } = props;
-
   return (
-    <button
-      type='button'
-      role='checkbox'
-      aria-checked={isChecked ? 'true' : 'false'}
-      data-test-id={`${testId}CheckboxButton`}
-    >
-      <label className={clsx('group flex cursor-pointer items-center justify-start gap-2', className)}>
-        <div className='flex items-center justify-center'>
-          <input
-            type='checkbox'
-            // value={isChecked} // <--- apparently you don't need value in a checkbox
-            checked={!!isChecked}
-            onChange={setIsChecked}
-            disabled={disabled}
-            aria-hidden='true'
-            tabIndex={-1}
-            className='size-0 opacity-0 pointer-events-none'
-            data-test-id={`${testId}Checkbox`}
-          />
-
-          <div className='flex justify-center items-center size-6 rounded-md border border-black group-has-[input:checked]:bg-blue-400 transition-all duration-500'>
-            <CheckIcon className='fill-white opacity-0 group-has-[input:checked]:opacity-100 transition-all duration-200' />
-          </div>
-        </div>
-
-        <div>{label}</div>
-      </label>
-    </button>
+    <CheckboxLogic {...props}>
+      <div className={styles.checkboxHolder}>
+        <CheckIcon className={styles.checkboxCheckMark} />
+      </div>
+    </CheckboxLogic>
   );
 }
---
+++ src/components/controls/Checkbox/CheckboxLogic/CheckboxLogic.tsx
@@ -0,0 +1,45 @@
+import type { PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import styles from './CheckboxLogic.module.scss';
+
+export type CheckboxLogicProps = PropsWithChildren<{
+  isChecked: boolean;
+  setIsChecked: (value: any) => void;
+  disabled?: boolean;
+  label?: string;
+  className?: string;
+  testId?: string;
+}>;
+
+export default function CheckboxLogic(props: CheckboxLogicProps) {
+  const { children, isChecked, setIsChecked, disabled, label, className, testId = '' } = props;
+
+  return (
+    <button
+      type='button'
+      role='checkbox'
+      aria-checked={isChecked ? 'true' : 'false'}
+      data-test-id={`${testId}CheckboxButton`}
+    >
+      <label className={clsx(styles.checkboxLabel, className)}>
+        <div className={styles.checkboxInputAndFakeCheckMark}>
+          <input
+            type='checkbox'
+            // value={isChecked} // <--- apparently you don't need value in a checkbox
+            checked={!!isChecked}
+            onChange={setIsChecked}
+            disabled={disabled}
+            aria-hidden='true'
+            tabIndex={-1}
+            className={styles.checkboxInput}
+            data-test-id={`${testId}Checkbox`}
+          />
+
+          {children}
+        </div>
+
+        <div>{label}</div>
+      </label>
+    </button>
+  );
+}
