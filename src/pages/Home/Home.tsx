b9e214d7723dabd3394715285672dda3c3e8a3e3
Tal Kohavy <talkohavy@gmail.com>
2025-04-07 20:15:02 +0300

    removed BroadcastChannel from Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,3 @@
-import { useEffect } from 'react';
-
-const bc = new BroadcastChannel('luckylove');
-
 export default function HomePage() {
-  bc.postMessage('New listening connected!');
-
-  useEffect(() => {
-    bc.addEventListener('message', (event) => {
-      console.log(event.data);
-      console.log(event);
-    });
-  }, []);
-
   return <div className='size-full p-6 overflow-auto'></div>;
 }
