9584ab71606bc1c75b4f8fe4c898b1b1c2e42936
Tal Kohavy <talkohavy@gmail.com>
2025-04-12 17:11:33 +0300

    temp

--- src/public/sw.js
+++ src/public/sw.js
@@ -3,6 +3,25 @@
 console.log('Service Worker script mounted!');
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('static').then((cache) => {
+      console.log('[Service Worker] caching app shell');
+      cache.addAll([
+        '/index.html',
+        'index-BWWZR0Od.css',
+        // '/',
+        // '/manifest.json',
+        // '/src/public/css/style.css',
+        // '/src/public/js/app.js',
+        // '/src/public/img/favicon.png',
+        // '/src/public/img/logo.png',
+        // '/src/public/img/hero.jpg',
+        // '/src/public/img/hero.webp',
+        // '/src/public/img/hero.avif',
+      ]);
+    }),
+  );
+
   console.log('[Service Worker] installing service worker...', event);
 });
 
@@ -12,12 +31,16 @@ self.addEventListener('activate', (event) => {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log('[Service Worker] fetching...', event);
+self.addEventListener('fetch', async (event) => {
+  try {
+    // console.log('[Service Worker] fetching...', event);
 
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      return response;
-    }),
-  );
+    const cacheHit = await caches.match(event.request);
+
+    const response = cacheHit ? cacheHit : fetch(event.request);
+
+    await event.respondWith(response);
+  } catch (error) {
+    console.error(error);
+  }
 });
