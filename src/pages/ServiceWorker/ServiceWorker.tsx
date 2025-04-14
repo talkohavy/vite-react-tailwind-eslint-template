8467a2c0a20b979d3be1b95b120d8e2d16b861b6
Tal Kohavy <talkohavy@gmail.com>
2025-04-14 09:32:49 +0300

    added cache on demand

--- src/pages/ServiceWorker/ServiceWorker.tsx
+++ src/pages/ServiceWorker/ServiceWorker.tsx
@@ -1,9 +1,48 @@
 import { useEffect, useState } from 'react';
+import { isCachesEnabled } from '../../common/utils/isCachesEnabled';
 import Button from '../../components/controls/Button';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
+const ON_DEMAND_CACHE = 'on-demand-cache';
+
 export default function ServiceWorker() {
   const [isVisible, setIsVisible] = useState(false);
+  const [cachedImageUrl, setCachedImageUrl] = useState<string | null>(null);
+
+  const onSaveClick = async () => {
+    const response = await fetch('/heart-256x256.png');
+
+    if (!response.ok) {
+      console.error('Failed to fetch resource:', response.statusText);
+      return;
+    }
+
+    if (isCachesEnabled()) cacheOnDemand(response);
+
+    const blob = await response.blob();
+    const objectUrl = URL.createObjectURL(blob);
+    setCachedImageUrl(objectUrl);
+  };
+
+  useEffect(() => {
+    async function loadCachedImageIfExists() {
+      if (!isCachesEnabled()) return;
+
+      try {
+        const resourceToCache = '/heart-256x256.png';
+        const cache = await caches.open(ON_DEMAND_CACHE);
+        const cachedResponse = await cache.match(resourceToCache);
+        if (cachedResponse) {
+          const objectUrl = URL.createObjectURL(await cachedResponse.blob());
+          setCachedImageUrl(objectUrl);
+        }
+      } catch (error) {
+        console.error('Failed to load cached resource:', error);
+      }
+    }
+
+    loadCachedImageIfExists();
+  }, []);
 
   useEffect(() => {
     // should essentially be called on main.tsx, but for the sake of this example, we are calling it here
@@ -31,6 +70,26 @@ export default function ServiceWorker() {
       </Button>
 
       {isVisible && <img src='/heart-256x256.png' alt='PWA Install Prompt' className='w-1/2 h-auto' />}
+
+      <Button className='mt-4' onClick={onSaveClick}>
+        Save
+      </Button>
+
+      {cachedImageUrl && (
+        <div className='mt-4'>
+          <div>Cached Image:</div>
+          <img src={cachedImageUrl} alt='Cached Resource' className='w-1/2 h-auto' />
+        </div>
+      )}
     </div>
   );
+}
+
+async function cacheOnDemand(response: Response) {
+  try {
+    const cache = await caches.open(ON_DEMAND_CACHE);
+    return cache.put(response.url, response.clone());
+  } catch (error) {
+    console.error('Failed to cache resource:', error);
+  }
 }
