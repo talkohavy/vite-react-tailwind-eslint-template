1095d9944db9eec3aca93f4dcb3692ca1d34d9d5
Tal Kohavy <talkohavy@gmail.com>
2025-04-21 23:09:15 +0300

    created a Picture element

---
+++ src/components/Picture/Picture.tsx
@@ -0,0 +1,54 @@
+import { type PropsWithChildren, useCallback, useState } from 'react';
+import { SIZES } from './logic/constants';
+
+type PictureProps = PropsWithChildren<{
+  srcSet: Array<string>;
+  alt: string;
+  className?: string;
+  imgClassName?: string;
+}>;
+
+/**
+ * @description
+ * Inside a <picture> element you can have:
+ *
+ * - Multiple <source> elements (optional)
+ * - One <img> element (required, acts as fallback)
+ *
+ * How it works:
+ *
+ * 1. The browser goes through the <source> elements from top to bottom.
+ * 2. It picks the first source that matches the current conditions (like media queries).
+ * 3. If no <source> matches (or browsers don't support <picture>), it falls back to the <img> element.
+ *
+ * Best Practices:
+ *
+ * - The src prop in the <img> tag should be your default/fallback image.
+ * - The src prop in the <img> tag should be the highest quality version.
+ * - The <img> tag will be used when none of the <source> conditions match.
+ * - The <img> tag will be used when browser doesn't support <picture>.
+ * - The <img> tag will be used when javascript is disabled.
+ * - A common pattern is to use the highest quality image as both the last item
+ *   in your `srcSet` array, and the src attribute of the <img> tag. This ensures
+ *   consistent fallback behavior and optimal image loading for different screen sizes.
+ *
+ */
+export default function Picture(props: PictureProps) {
+  const { children, alt, srcSet, className, imgClassName } = props;
+
+  const [imageError, setImageError] = useState(false);
+
+  const onError = useCallback(() => setImageError(true), []);
+
+  if (imageError) return children;
+
+  return (
+    <picture className={className}>
+      {srcSet.map((src, index) => (
+        <source key={index} srcSet={src} media={SIZES[index]} />
+      ))}
+
+      <img loading='lazy' src={SIZES.at(-1)} alt={alt} onError={onError} className={imgClassName} />
+    </picture>
+  );
+}
---
+++ src/components/Picture/logic/constants.ts
@@ -0,0 +1,6 @@
+export const SIZES = [
+  '(max-width: 320px)', // Mobile
+  '(max-width: 768px)', // Tablet
+  '(max-width: 1024px)', // Desktop
+  undefined, // Big Screens
+];
