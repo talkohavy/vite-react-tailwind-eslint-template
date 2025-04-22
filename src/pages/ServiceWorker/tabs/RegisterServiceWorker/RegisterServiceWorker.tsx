9b9c5c5476b67c6059248592a18ff8949303a6fb
Tal Kohavy <talkohavy@gmail.com>
2025-04-23 00:43:55 +0300

    split ServiceWorkerPage into Tabs

---
+++ src/pages/ServiceWorker/tabs/RegisterServiceWorker/RegisterServiceWorker.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { registerServiceWorker } from '../../logic/utils/registerServiceWorker';
+
+export default function RegisterServiceWorker() {
+  useEffect(() => {
+    // should essentially be called on main.tsx, but for the sake of this example, we are calling it here
+    registerServiceWorker();
+  }, []);
+
+  return (
+    <div className='size-full p-6 overflow-auto'>
+      <div>Service Worker Tutorial</div>
+
+      <div className='border rounded-md p-2 mt-4'>
+        This page should be loaded from the service worker, even if you're offline.
+      </div>
+    </div>
+  );
+}
