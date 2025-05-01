42de7ba8e0089e0366e8e8d96313299703a4f42f
Tal Kohavy <talkohavy@gmail.com>
2025-05-01 19:00:40 +0300

    the service worker script is now an init function

--- src/lib/ServiceWorker/initScript.ts
+++ src/lib/ServiceWorker/initScript.ts
@@ -3,7 +3,11 @@ import { MyServiceWorker } from './ServiceWorker';
 
 const assetManager = new AssetManager();
 
-MyServiceWorker.getInstance(self);
-MyServiceWorker.addOnInstalListener(assetManager.cacheStaticAssets.bind(assetManager));
-MyServiceWorker.addOnActivateListener(assetManager.cleanUpOldCaches.bind(assetManager));
-MyServiceWorker.addOnFetchListener(assetManager.cacheWithNetworkFallbackStrategy.bind(assetManager));
+async function initServiceWorker() {
+  MyServiceWorker.getInstance(self);
+  MyServiceWorker.addOnInstalListener(assetManager.cacheStaticAssets.bind(assetManager));
+  MyServiceWorker.addOnActivateListener(assetManager.cleanUpOldCaches.bind(assetManager));
+  MyServiceWorker.addOnFetchListener(assetManager.cacheWithNetworkFallbackStrategy.bind(assetManager));
+}
+
+initServiceWorker();
