18b873695970b8607320dd4abf3724958b77e12d
Tal Kohavy <talkohavy@gmail.com>
2025-05-03 16:45:52 +0300

    Button: onClick has an event

--- src/components/controls/Button/Button.tsx
+++ src/components/controls/Button/Button.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type ButtonProps = PropsWithChildren<{
-  onClick: () => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   className?: string;
   disabled?: boolean;
 }>;
