b1fe24cfed2816e6fa4a6b78d00984bcb147c818
Tal Kohavy <talkohavy@gmail.com>
2025-07-17 10:34:14 +0300

    ran ncuuuuu

--- src/global.d.ts
+++ src/global.d.ts
@@ -34,3 +34,8 @@ declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '@tailwindcss/vite' {
+  function tailwindcss(options?: any): any;
+  export default tailwindcss;
+}
