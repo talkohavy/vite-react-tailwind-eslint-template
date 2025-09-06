791ce12ee618a3823eaceb8d300a9396b19355b3
Tal Kohavy <talkohavy@gmail.com>
2025-09-06 11:33:03 +0300

    CursorPosition

--- src/lib/QueryLanguage/ContextAnalyzer/logic/constants.ts
+++ src/lib/QueryLanguage/ContextAnalyzer/logic/constants.ts
@@ -1,8 +1,10 @@
 export const ContextTypes = {
   Key: 'key',
   Value: 'value',
+  QuotedString: 'quoted-string',
   LogicalOperator: 'operator',
   Comparator: 'comparator',
+  Colon: 'colon',
   LeftParenthesis: 'left-parenthesis',
   RightParenthesis: 'right-parenthesis',
 } as const;
@@ -10,3 +12,18 @@ export const ContextTypes = {
 type TypeOfContextType = typeof ContextTypes;
 export type ContextTypeKeys = keyof TypeOfContextType;
 export type ContextTypeValues = TypeOfContextType[ContextTypeKeys];
+
+export const CursorPosition = {
+  InKey: 'in-key',
+  InValue: 'in-value',
+  InOperator: 'in-operator',
+  InComparator: 'in-comparator',
+  AfterValue: 'after-value',
+  BetweenKeyComparator: 'between-key-comparator',
+  BetweenComparatorValue: 'between-comparator-value',
+  Unknown: 'unknown',
+} as const;
+
+type TypeOfCursorPosition = typeof CursorPosition;
+export type CursorPositionKeys = keyof TypeOfCursorPosition;
+export type CursorPositionValues = TypeOfCursorPosition[CursorPositionKeys];
