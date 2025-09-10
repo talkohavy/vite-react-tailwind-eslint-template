49d749ec79af964fa42f2aa8ed866bde54f1d799
Tal Kohavy <talkohavy@gmail.com>
2025-09-10 09:14:44 +0300

    small

--- src/lib/QueryLanguage/ASTBuilder/index.ts
+++ src/lib/QueryLanguage/ASTBuilder/index.ts
@@ -1,5 +1,9 @@
+// Main export
 export { ASTBuilder } from './ASTBuilder';
+
+// constants
 export { AstTypes } from './logic/constants';
 
+// re-export types
 export type { AstTypeKeys, AstTypeValues } from './logic/constants';
 export type * from './types';
