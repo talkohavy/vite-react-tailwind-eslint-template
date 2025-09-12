d1d2bd70f69b84434901b27a39beedcddcca20a7
Tal Kohavy <talkohavy@gmail.com>
2025-09-12 03:52:13 +0300

    showcase page more friendly now

--- src/pages/Showcase/Showcase.tsx
+++ src/pages/Showcase/Showcase.tsx
@@ -8,10 +8,10 @@ import ToggleV2 from '../../components/beautiful/ToggleV2';
 import ToggleV3 from '../../components/beautiful/ToggleV3';
 import ToggleV4 from '../../components/beautiful/ToggleV4';
 import CodeBlock from '../../components/CodeBlock';
+import Combobox from '../../components/controls/Autocomplete';
 import Checkbox from '../../components/controls/Checkbox';
 import Tag from '../../components/controls/Checkbox/Tag';
 import ColorPicker from '../../components/controls/ColorPicker';
-import Combobox from '../../components/controls/Combobox';
 import Input from '../../components/controls/Input';
 import NumberInput from '../../components/controls/NumberInput';
 import RadioDots from '../../components/controls/RadioGroup/RadiosDots';
@@ -31,7 +31,7 @@ import { useScrollToEdge } from '../../hooks/useScrollToEdge';
 import DropdownMenuContent from './components/DropdownMenuContent';
 import { useRunningProgress } from './components/useRunningProgress';
 
-const opt = Array.from(Array(4).keys()).map((_, index) => ({ value: index || 'a', label: index.toString() }));
+const opt = Array.from(Array(40).keys()).map((_, index) => ({ value: index || 'a', label: index.toString() }));
 
 const options: Array<SelectOption> = [
   ...opt,
@@ -52,7 +52,7 @@ export default function RadixComponents() {
   const [selectComboboxOption, setSelectComboboxOption] = useState<any>(options[4]);
   const [color, setColor] = useState(() => parseColor('hsl(20, 100%, 50%)'));
   const [date, setDate] = useState([parseDate('2022-01-01')]);
-  const [pinInput, setPinInput] = useState<string>('');
+  const [_pinInput, setPinInput] = useState<string>('');
   const [selectedRadio, setSelectedRadio] = useState<any>(null);
   const [textareaValue, setTextareaValue] = useState<string>('');
   const [numberValue, setNumberValue] = useState<number>(0);
@@ -68,8 +68,6 @@ export default function RadixComponents() {
 
   const { isCloseToEdge: isCloseToTop, onScroll: onScrollToTop } = useIsCloseToEdge({ to: 'top' });
   const { scrollToEdge: scrollToTop } = useScrollToEdge({ refElement: refElement, to: 'top' });
-
-  console.log('pinInput is:', pinInput);
 
   return (
     <div
@@ -89,108 +87,190 @@ export default function RadixComponents() {
           <DownArrow className='size-3' />
         </button>
       )}
+      <div className='font-mono flex flex-col gap-2 w-full'>
+        <div>Input:</div>
 
-      <Input initialValue={inputValue} onChange={setInputValue} placeholder='Enter name' />
+        <Input
+          initialValue={inputValue}
+          onChange={setInputValue}
+          placeholder='Enter name'
+          className='dark:bg-slate-950 px-4 max-w-xs shadow-xs dark:shadow-dark-xs'
+        />
+      </div>
+      <div className='font-mono flex flex-col gap-2 w-full'>
+        <div>RadioDots:</div>
 
-      <RadioDots
-        options={[
-          { value: 0, label: '0' },
-          { value: 1, label: '2' },
-        ]}
-        value={selectedRadio}
-        setValue={setSelectedRadio}
-      />
+        <RadioDots
+          options={[
+            { value: 0, label: '0' },
+            { value: 1, label: '2' },
+          ]}
+          value={selectedRadio}
+          setValue={setSelectedRadio}
+          className='flex gap-2'
+        />
+      </div>
+      <div className='font-mono flex flex-col gap-2 w-full'>
+        <div>ProgressBar:</div>
 
-      <ProgressBar completed={progressBarValue} className='shrink-0 h-12 w-full' />
+        <ProgressBar completed={progressBarValue} className='shrink-0 h-12 w-full' />
+      </div>
+      <div className='font-mono flex flex-col gap-2 w-full'>
+        <div>FancyProgressBar:</div>
 
-      <FancyProgressBar completed={progressBarValue} className='shrink-0 h-12 w-full' />
+        <FancyProgressBar completed={progressBarValue} className='shrink-0 h-12 w-full max-w-lg' />
+      </div>
+      <div className='font-mono flex flex-col gap-2 w-full'>
+        <div>PinInput:</div>
 
-      <PinInput
-        pinLength={4}
-        onDone={(props) => setPinInput(props.valueAsString)}
-        selectOnFocus
-        blurOnComplete
-        required
-        // disabled
-        // placeholder='_'
-        // isOtp
-        // isSecureMask
-        // autoFocus
-        // defaultValue={['1', '2', '3', '4']}
-      />
+        <PinInput
+          pinLength={4}
+          onDone={(props) => setPinInput(props.valueAsString)}
+          selectOnFocus
+          blurOnComplete
+          required
+          // disabled
+          // placeholder='_'
+          // isOtp
+          // isSecureMask
+          // autoFocus
+          // defaultValue={['1', '2', '3', '4']}
+        />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>DatePicker:</div>
 
-      <DatePicker value={date} setValue={setDate} />
+        <DatePicker value={date} setValue={setDate} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>ColorPicker:</div>
 
-      <ColorPicker color={color} setColor={setColor} />
+        <ColorPicker color={color} setColor={setColor} className='max-w-xs' />
 
-      <div>
-        <div>hex color: {color.toString('hex')}</div>
-        <div>rgba color: {color.toString('rgba')}</div>
-        <div>rgba color: {color.toString('hsla')}</div>
+        <ul className='px-4'>
+          <li>• hex color: {color.toString('hex')}</li>
+          <li>• rgba color: {color.toString('rgba')}</li>
+          <li>• rgba color: {color.toString('hsla')}</li>
+        </ul>
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>NumberInput:</div>
+
+        <NumberInput value={numberValue} setValue={setNumberValue} min={0} max={12} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Toggle:</div>
+
+        <Toggle isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>ToggleV1:</div>
+
+        <ToggleV1 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>ToggleV2:</div>
+
+        <ToggleV2 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>ToggleV3:</div>
+
+        <ToggleV3 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>ToggleV4:</div>
+
+        <ToggleV4 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Checkbox:</div>
+
+        <Checkbox isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} label='Remember me?' />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Tag:</div>
+
+        <Tag isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} label='Bicycle' />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Textarea:</div>
+
+        <Textarea
+          value={textareaValue}
+          setValue={(e) => setTextareaValue(e.target.value)}
+          className='dark:bg-slate-950'
+        />
+      </div>
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Select:</div>
+
+        <Select
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+          options={options}
+          placeholder='choose one'
+          className='max-w-xs'
+          dropdownClassName='max-h-60'
+          // showArrow
+          // itemClassName='bg-red-500 data-[state=checked]:bg-blue-500'
+        />
       </div>
 
-      <NumberInput value={numberValue} setValue={setNumberValue} min={0} max={12} />
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Combobox:</div>
 
-      <Toggle isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
-      <ToggleV1 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
-      <ToggleV2 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
-      <ToggleV3 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
-      <ToggleV4 isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} />
-
-      <Checkbox isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} label='Remember me?' />
-
-      <Tag isChecked={isChecked} setIsChecked={() => setIsChecked((prev) => !prev)} label='Bicycle' />
-
-      <Textarea value={textareaValue} setValue={(e) => setTextareaValue(e.target.value)} />
-
-      <Select
-        selectedOption={selectedOption}
-        setSelectedOption={setSelectedOption}
-        options={options}
-        placeholder='choose one'
-        dropdownClassName='max-h-60'
-        // showArrow
-        // itemClassName='bg-red-500 data-[state=checked]:bg-blue-500'
-      />
-
-      <Combobox
-        selectOption={selectComboboxOption}
-        setSelectOption={setSelectComboboxOption}
-        options={options}
-        placeholder='Choose...'
-        dropdownClassName='h-44'
-        loop
-      />
-
-      <DropdownMenu
-        triggerElement={
-          <button type='button' className='!border !border-white !p-4 !rounded-lg'>
-            Open Dropdown
-          </button>
-        }
-        isOpen={isDropDownMenuOpen}
-        setIsOpen={setIsDropDownMenuOpen}
-        showArrow
-      >
-        <DropdownMenuContent
-          bookmarksChecked={bookmarksChecked}
-          setBookmarksChecked={setBookmarksChecked}
-          urlsChecked={urlsChecked}
-          setUrlsChecked={setUrlsChecked}
-          person={person}
-          setPerson={setPerson}
+        <Combobox
+          selectOption={selectComboboxOption}
+          setSelectOption={setSelectComboboxOption}
+          options={options}
+          placeholder='Choose...'
+          className='max-w-xs dark-bg-red shadow-xs dark:shadow-dark-xs'
+          dropdownClassName='max-h-[224px] overflow-auto'
+          loop
         />
-      </DropdownMenu>
+      </div>
 
-      <TooltipTrigger groupId={tooltipUniqueId} contentOverride={'spelling'} className='w-auto inline-block'>
-        <div className='border border-black p-4 rounded-lg dark:border-white w-auto'>My Tooltip</div>
-      </TooltipTrigger>
-      <Tooltip groupId={tooltipUniqueId} place={Placement.Top} isClickable className='!p-3' />
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>DropdownMenu:</div>
 
-      <div>
-        <CodeBlock
-          language='typescript'
-          code={`'use strict'
+        <DropdownMenu
+          triggerElement={
+            <button type='button' className='!border !border-white !p-4 !rounded-lg'>
+              Open Dropdown
+            </button>
+          }
+          isOpen={isDropDownMenuOpen}
+          setIsOpen={setIsDropDownMenuOpen}
+          showArrow
+        >
+          <DropdownMenuContent
+            bookmarksChecked={bookmarksChecked}
+            setBookmarksChecked={setBookmarksChecked}
+            urlsChecked={urlsChecked}
+            setUrlsChecked={setUrlsChecked}
+            person={person}
+            setPerson={setPerson}
+          />
+        </DropdownMenu>
+      </div>
+
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>Tooltip:</div>
+
+        <TooltipTrigger groupId={tooltipUniqueId} contentOverride={'spelling'} className='w-auto inline-block'>
+          <div className='border border-black p-4 rounded-lg dark:border-white w-auto'>My Tooltip</div>
+        </TooltipTrigger>
+        <Tooltip groupId={tooltipUniqueId} place={Placement.Top} isClickable className='!p-3' />
+      </div>
+
+      <div className='font-mono flex flex-col justify-start gap-2 w-full'>
+        <div>CodeBlock:</div>
+
+        <div>
+          <CodeBlock
+            language='typescript'
+            code={`'use strict'
 
 import { useMemo, useEffect } from 'react';
 import { convertReactPropsToRevealProps } from '../utils/convertReactPropsToRevealProps';
@@ -211,13 +291,13 @@ class Person {
         this.name = name;
     }
 }`}
-        />
+          />
+        </div>
       </div>
 
       <div className='w-96 h-10'>
         <CodeBlock code={'ls --name hello'} />
       </div>
-
       {!isCloseToTop && (
         <button
           type='button'
