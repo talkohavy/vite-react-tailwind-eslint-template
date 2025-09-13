f3f6146dbd3bbb3dcacae4e2c115a4194b859473
Tal Kohavy <talkohavy@gmail.com>
2025-09-13 22:48:34 +0300

    removed QueryLanguage as lib

--- src/lib/QueryLanguage/ASTBuilder/index.ts
+++
@@ -1,9 +0,0 @@
-// Main export
-export { ASTBuilder } from './ASTBuilder';
-
-// constants
-export { AstTypes } from './logic/constants';
-
-// re-export types
-export type { AstTypeKeys, AstTypeValues } from './logic/constants';
-export type * from './types';
--- src/lib/QueryLanguage/QueryParser/index.ts
+++
@@ -1,6 +0,0 @@
-export { QueryParser } from './QueryParser';
-
-export { ContextTypes } from './logic/constants';
-
-export type * from './logic/constants';
-export type * from './types';
