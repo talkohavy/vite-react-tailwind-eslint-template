7233d1ad42c7722617844e3fc6bbd58976c25c6a
talkohavy <talkohavy@gmail.com>
2025-10-31 09:50:34 +0200

    TreeView: testIds fixed, dataAttributes fixed, css classes added

--- src/components/TreeView/TreeNodeItem/content/ExpandButton/ExpandButton.tsx
+++ src/components/TreeView/TreeNodeItem/content/ExpandButton/ExpandButton.tsx
@@ -5,13 +5,21 @@ type ExpandButtonProps = {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled: boolean;
   className?: string;
+  testId?: string;
 };
 
 export default function ExpandButton(props: ExpandButtonProps) {
-  const { onClick, disabled, className } = props;
+  const { onClick, disabled, className, testId = '' } = props;
 
   return (
-    <button type='button' onClick={onClick} disabled={disabled} className={className}>
+    <button
+      type='button'
+      onClick={onClick}
+      disabled={disabled}
+      data-expand-button=''
+      className={className}
+      data-test-id={`${testId}-expand-button`}
+    >
       <RightArrow className={styles.btnArrow} />
     </button>
   );
