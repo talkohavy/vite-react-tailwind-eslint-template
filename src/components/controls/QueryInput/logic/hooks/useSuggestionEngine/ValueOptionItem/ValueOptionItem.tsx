f1c277f509c678a9704ab21c77585ec6cf634796
Tal Kohavy <talkohavy@gmail.com>
2025-11-20 07:55:33 +0200

    hell if i know what changed

--- src/components/controls/QueryInput/logic/hooks/useSuggestionEngine/ValueOptionItem/ValueOptionItem.tsx
+++ src/components/controls/QueryInput/logic/hooks/useSuggestionEngine/ValueOptionItem/ValueOptionItem.tsx
@@ -1,17 +1,17 @@
 import styles from './ValueOptionItem.module.scss';
 
 type ValueOptionItemProps = {
-	keyName: string;
-	value: string;
+  keyName: string;
+  value: string;
 };
 
 export default function ValueOptionItem(props: ValueOptionItemProps) {
-	const { keyName, value } = props;
+  const { keyName, value } = props;
 
-	return (
-		<div className={styles.valueOptionItem}>
-			<div className={styles.key}>{keyName}:</div>
-			<div>{value}</div>
-		</div>
-	);
+  return (
+    <div className={styles.valueOptionItem}>
+      <div className={styles.key}>{keyName}:</div>
+      <div>{value}</div>
+    </div>
+  );
 }
