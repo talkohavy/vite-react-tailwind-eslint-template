a71defccd14f97813a306e00379c0427cc1963f3
Tal Kohavy <talkohavy@gmail.com>
2025-12-01 20:52:56 +0200

    ServerCall now has an example with abort

--- src/pages/ServerCall/ServerCall.tsx
+++ src/pages/ServerCall/ServerCall.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import LoadingBumpingBalls from '../../components/beautiful/LoadingBumpingBalls';
 import Button from '../../components/controls/Button';
 import { httpClient } from '../../lib/HttpClient';
 
 export default function ServerCall() {
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<any>();
   const [error, setError] = useState<any>();
+  const abortRef = useRef<(() => void) | null>(null);
 
   const fetchUsers = async () => {
     try {
-      const data = await httpClient.get('/users').promise;
+      const data = await httpClient.get('/api/users').promise;
 
       console.log('response is:', data);
 
@@ -20,29 +23,39 @@ export default function ServerCall() {
 
   const createUser = async () => {
     try {
-      const data = await httpClient.post('/users', {
+      setIsLoading(true);
+      const { promise, abort } = httpClient.post('/api/users', {
         body: {
           name: 'John Doe',
           email: 'talkohavy@example.com',
         },
-      }).promise;
+      });
+
+      abortRef.current = abort;
+      const data = await promise;
 
       console.log('response is:', data);
     } catch (error) {
       setError(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className='flex flex-col gap-6 size-full p-4'>
-      <div>data: {data && JSON.stringify(data)}</div>
-      <div>error: {error?.message}</div>
-
       <div className='flex gap-2'>
         <Button onClick={fetchUsers}>Fetch Users</Button>
 
         <Button onClick={createUser}>Create User</Button>
+
+        <Button onClick={() => abortRef.current?.()}>Abort</Button>
       </div>
+
+      {isLoading && <LoadingBumpingBalls />}
+
+      <div>data: {data && JSON.stringify(data)}</div>
+      <div>error: {error?.message}</div>
     </div>
   );
 }
