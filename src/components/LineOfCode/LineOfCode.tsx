f897c7a6e138f33c8679b88e479145d4f2204e52
Tal Kohavy <talkohavy@gmail.com>
2026-02-28 16:23:15 +0200

    created LineOfCode component

---
+++ src/components/LineOfCode/LineOfCode.tsx
@@ -0,0 +1,9 @@
+type LineOfCodeProps = {
+  text: string;
+};
+
+export default function LineOfCode(props: LineOfCodeProps) {
+  const { text } = props;
+
+  return <code className='rounded bg-gray-200 px-1 py-0.5 dark:bg-gray-700'>{text}</code>;
+}
