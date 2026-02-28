dd020dd7ea8a2416e75c7ff3222882191a784ed7
Tal Kohavy <talkohavy@gmail.com>
2026-02-28 09:21:24 +0200

    made sidebar thinner, gave button cursor-pointer

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -9,25 +9,27 @@ export default function Sidebar() {
   return (
     <nav
       className={clsx(
-        'relative flex h-full shrink-0 flex-col items-start justify-start bg-amber-50 p-10 shadow-md transition-all duration-300 dark:bg-slate-500',
-        isSidebarOpen ? 'w-72' : 'w-4',
+        'relative flex h-full shrink-0 flex-col items-start justify-start bg-amber-50 shadow-md transition-all duration-300 dark:bg-slate-500',
+        isSidebarOpen ? 'w-72' : 'w-10',
       )}
     >
-      <button
-        type='button'
-        onClick={() => setIsSidebarOpen((prev) => !prev)}
-        className='absolute right-0 top-1/2 size-8 translate-x-1/2 rounded-full bg-blue-400 p-2 opacity-20 hover:opacity-100'
-      >
-        <DownArrow className={clsx('stroke-black', isSidebarOpen ? 'rotate-90' : 'rotate-270')} />
-      </button>
+      <div className='size-full p-4 overflow-auto'>
+        <button
+          type='button'
+          onClick={() => setIsSidebarOpen((prev) => !prev)}
+          className='absolute right-0 top-1/2 size-8 cursor-pointer translate-x-1/2 rounded-full bg-blue-400 p-2 opacity-20 hover:opacity-100'
+        >
+          <DownArrow className={clsx('stroke-black', isSidebarOpen ? 'rotate-90' : 'rotate-270')} />
+        </button>
 
-      <div
-        className={clsx(
-          'w-full opacity-0 transition-opacity duration-300 delay-300 overflow-auto',
-          isSidebarOpen && 'opacity-100',
-        )}
-      >
-        {isSidebarOpen && <SideBarLinkList />}
+        <div
+          className={clsx(
+            'w-full opacity-0 transition-opacity duration-300 delay-300 overflow-auto',
+            isSidebarOpen && 'opacity-100',
+          )}
+        >
+          {isSidebarOpen && <SideBarLinkList />}
+        </div>
       </div>
     </nav>
   );
