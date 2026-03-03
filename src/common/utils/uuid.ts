2d627870f9d196bcfb7a88d6795359889547e879
Tal Kohavy <talkohavy@gmail.com>
2026-03-03 21:26:41 +0200

    uuid: crypto.randomUUID - defend against http

--- src/common/utils/uuid.ts
+++ src/common/utils/uuid.ts
@@ -1,3 +1,7 @@
 export function uuid() {
+  if (typeof crypto.randomUUID === 'undefined') {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2, 15);
+  }
+
   return crypto.randomUUID();
 }
