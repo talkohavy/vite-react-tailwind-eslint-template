20559410168f81443849cb55910224804b3ba3a2
Tal Kohavy <talkohavy@gmail.com>
2026-03-03 21:00:45 +0200

    LineOfCode supports className

--- src/components/LineOfCode/LineOfCode.tsx
+++ src/components/LineOfCode/LineOfCode.tsx
@@ -1,9 +1,14 @@
+import clsx from 'clsx';
+
 type LineOfCodeProps = {
   text: string;
+  className?: string;
 };
 
 export default function LineOfCode(props: LineOfCodeProps) {
-  const { text } = props;
+  const { text, className } = props;
 
-  return <code className='rounded bg-gray-100 px-1 py-0.5 dark:bg-gray-700'>{text}</code>;
+  return (
+    <code className={clsx('rounded bg-gray-100 px-1 py-0.5 dark:bg-gray-700 text-gray-100', className)}>{text}</code>
+  );
 }
