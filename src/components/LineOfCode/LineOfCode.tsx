2c7491c7c6777e05a004ae3ae057fb4cfa31108e
Tal Kohavy <talkohavy@gmail.com>
2026-03-03 19:03:49 +0200

    LineOfCode: brighter bg color on light mode

--- src/components/LineOfCode/LineOfCode.tsx
+++ src/components/LineOfCode/LineOfCode.tsx
@@ -5,5 +5,5 @@ type LineOfCodeProps = {
 export default function LineOfCode(props: LineOfCodeProps) {
   const { text } = props;
 
-  return <code className='rounded bg-gray-200 px-1 py-0.5 dark:bg-gray-700'>{text}</code>;
+  return <code className='rounded bg-gray-100 px-1 py-0.5 dark:bg-gray-700'>{text}</code>;
 }
