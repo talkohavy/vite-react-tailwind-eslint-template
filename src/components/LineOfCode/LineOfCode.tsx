a7109aa369d7685cc3b73d7eed64fa38ab566364
Tal Kohavy <talkohavy@gmail.com>
2026-03-04 21:21:01 +0200

    LineOfCode: fixed bug of color

--- src/components/LineOfCode/LineOfCode.tsx
+++ src/components/LineOfCode/LineOfCode.tsx
@@ -9,6 +9,14 @@ export default function LineOfCode(props: LineOfCodeProps) {
   const { text, className } = props;
 
   return (
-    <code className={clsx('rounded bg-gray-100 px-1 py-0.5 dark:bg-gray-700 text-gray-100', className)}>{text}</code>
+    <code
+      className={clsx(
+        'rounded-md bg-gray-100 px-1 py-0.5 dark:bg-gray-700 text-black dark:text-gray-100 w-fit',
+        className,
+      )}
+      style={{ fontFamily: "Menlo, Monaco, 'Courier New', monospace" }}
+    >
+      {text}
+    </code>
   );
 }
