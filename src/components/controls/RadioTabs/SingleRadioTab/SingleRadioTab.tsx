ad9048ddce06f94af691a8293cee4a7f2d080462
Tal Kohavy <talkohavy@gmail.com>
2026-03-04 08:12:45 +0200

    SingleRadioTab: made styles more clear

--- src/components/controls/RadioTabs/SingleRadioTab/SingleRadioTab.tsx
+++ src/components/controls/RadioTabs/SingleRadioTab/SingleRadioTab.tsx
@@ -1,3 +1,10 @@
+import clsx from 'clsx';
+
+const base = 'relative flex justify-center items-center h-8 w-32 rounded-t-md cursor-pointer';
+const lightMode =
+  'text-gray-700 bg-gray-200 group-has-[input:checked]/radioWrapper:text-white group-has-[input:checked]/radioWrapper:bg-blue-500 group-has-[input:focus:not(:checked)]/radioWrapper:border-blue-400 group-has-[input:hover:not(:disabled)]/radioWrapper:bg-blue-400 group-has-[input:hover:not(:disabled)]/radioWrapper:text-white group-has-[input:disabled]/radioWrapper:bg-gray-400 group-has-[input:disabled]/radioWrapper:opacity-40';
+const darkMode = 'dark:text-gray-100 dark:bg-gray-500';
+
 type SingleRadioTabProps = {
   label: string;
 };
@@ -5,9 +12,5 @@ type SingleRadioTabProps = {
 export default function SingleRadioTab(props: SingleRadioTabProps) {
   const { label } = props;
 
-  return (
-    <div className='flex justify-center items-center relative bg-neutral-500 group-has-[input:checked]/radioWrapper:bg-blue-500 group-has-[input:focus:not(:checked)]/radioWrapper:border-blue-400 group-has-[input:hover:not(:disabled)]/radioWrapper:bg-blue-400 group-has-[input:disabled]/radioWrapper:bg-neutral-400 group-has-[input:disabled]/radioWrapper:opacity-40 h-8 w-32 rounded-t-md cursor-pointer'>
-      {label}
-    </div>
-  );
+  return <div className={clsx(base, lightMode, darkMode)}>{label}</div>;
 }
