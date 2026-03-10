23a4a63aa0417c8640b516b2b59cc6ee1199cda4
Tal Kohavy <talkohavy@gmail.com>
2026-03-10 22:45:48 +0200

    extracted route type to a separate file

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,17 +1,7 @@
-import type { LazyExoticComponent, ReactNode } from 'react';
-
-export type Route = {
-  to: string;
-  text: string;
-  activeNames: Array<string>;
-  Component: (() => any) | LazyExoticComponent<() => ReactNode>;
-  hideFromSidebar?: boolean;
-  children?: Array<Route>;
-};
-
 export type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 
 export type * from './postMessageEvents';
 export type * from './backgroundSync';
+export type * from './route';
---
+++ src/common/types/route.ts
@@ -0,0 +1,10 @@
+import type { LazyExoticComponent, ReactNode } from 'react';
+
+export type Route = {
+  to: string;
+  text: string;
+  activeNames: Array<string>;
+  Component: (() => any) | LazyExoticComponent<() => ReactNode>;
+  hideFromSidebar?: boolean;
+  children?: Array<Route>;
+};
