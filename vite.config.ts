e2f6d8718d091d1b2a6b8a0e4165910cad27ebfe
Tal Kohavy <talkohavy@gmail.com>
2026-03-13 10:05:01 +0200

    repalced @vitejs/plugin-react-swc with @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 // import basicSsl from '@vitejs/plugin-basic-ssl';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
