bf4b518613c72c72748d4bd75e96b44872aa67a7
Tal Kohavy <talkohavy@gmail.com>
2026-05-19 22:23:29 +0300

    updated the global.d.ts file

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
 
+/**
+ * This following svg import only works if this file doesn't include:
+ * - A global `import` statement
+ * - An `export {}` statement
+ *
+ * If it does, then the following code will work in runtime, but will produce a type error:
+ * import { ReactComponent as ViteIconComponent } from '@src/assets/vite2.svg';
+ */
 declare module '*.svg' {
   // 1. When config is set to: exportType: 'default':
   // const content: React.FC<React.SVGProps<SVGElement>>;
