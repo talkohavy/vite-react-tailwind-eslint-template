69e6c746e668cfd78be8eddc17aad167eeec25d2
Tal Kohavy <talkohavy@gmail.com>
2026-06-06 08:57:05 +0300

    styles of syntax highlighter should by inside common/styles

--- src/common/bootstrap.ts
+++ src/common/bootstrap.ts
@@ -1,4 +1,6 @@
 import '../lib/syntaxHighlighter';
+import './styles/syntax-highlighter/1c-light.css';
+import './styles/syntax-highlighter/vscode-dark.css';
 import './styles/root.css';
 import './styles/InputWithDropdown.css';
 import './styles/table-dark-mode.css';
--- src/lib/syntaxHighlighter/index.ts
+++ src/lib/syntaxHighlighter/index.ts
@@ -1,5 +1,3 @@
-import './styles/1c-light.css';
-import './styles/vscode-dark.css';
 import hljs from 'highlight.js';
 import { javascriptLanguageToRegister } from './languages/javascript';
 import { typescriptLanguageToRegister } from './languages/typescript';
